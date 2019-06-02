a88cb63f27e1f03497f88761c5099663049a4690
Helene <1810738627@stud.fh-kufstein.ac.at>
2019-06-02 11:27:30 +0200

    serviceworker and webmanifest adjusted

--- docs/sw.js
+++ docs/sw.js
@@ -3,7 +3,7 @@ self.addEventListener('install', (event) => {
     event.waitUntil(
         caches.open(swCache)
             .then(cache => cache.addAll([
-                '/simonpwa/',
+                '/SimonGamePWAv2/',
                 'index.html',                
                 'app.js',
                 'css/styles.css',
